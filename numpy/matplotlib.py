927a0b755abcfd98a3fdc07e719dc39da73453d5
dubter <Myadressid14@gmail.com>
2023-02-26 00:42:29 +0300

    task3 - about simple data analysis

--- numpy/matplotlib.py
+++ numpy/matplotlib.py
@@ -1,12 +1,32 @@
-import numpy as np
-import matplotlib.pyplot as plt
+def knn(features, train_features, train_target, k=1):
+    '''
+    Предсказание целевого признака для данных features -- pandas-таблица данных.
+    на основе обучающего набора данных train_features, train_target.
+    Возвращает pandas.Series с теми же индексами, что и у features.
+    Число k -- количество соседей.
+    '''
 
-y = np.zeros((10, 10))
-for i in range(10):
-    for j in range(10):
-        if (i + j) % 2 == 0:
-            y[i][j] = 0
-        else:
-            y[i][j] = 1
-plt.imshow(y)
-plt.show()
+    predicted = pd.Series(index=features.index)
+
+    # Заменяем строки на числа если колонка 'Sex' существует
+    try:
+        features['Sex'][features['Sex'] == 'male'] = 0
+        features['Sex'][features['Sex'] == 'female'] = 1
+        train_features['Sex'][train_features['Sex'] == 'male'] = 0
+        train_features['Sex'][train_features['Sex'] == 'female'] = 1
+    except:
+        pass
+
+    # Ищем ближайшие k точек из train_features для каждой точки из features
+    nearest_points = find_nearest_points(features.values, train_features.values, k)
+
+    # Для каждой точки из features
+    for i in range(features.shape[0]):
+        # Находим индексы k ближайших точек из train_features
+        nearest_points_indices = nearest_points[i]
+        # Находим значения целевого признака для найденных точек
+        nearest_points_target = train_target.iloc[nearest_points_indices]
+        # Выбираем наиболее часто встречающееся значение целевого признака
+        predicted.iloc[i] = nearest_points_target.value_counts().index[0]
+
+    return predicted
